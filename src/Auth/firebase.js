22dedcad49e891084418173ec877d72aa18e75d3
AlanContreras784 <alancontreras784@gmail.com>
2025-07-17 00:34:51 -0300

    archivo env

--- src/Auth/firebase.js
+++ src/Auth/firebase.js
@@ -9,13 +9,13 @@ import { addDoc, collection, deleteDoc, doc, getDoc, getDocs, getFirestore, setD
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCgaPRB1RWsJ02UmkeOwAFI9CLSpTm9xcs",
-    authDomain: "prueba-auth1-5a565.firebaseapp.com",
-    projectId: "prueba-auth1-5a565",
-    storageBucket: "prueba-auth1-5a565.firebasestorage.app",
-    messagingSenderId: "1089570942852",
-    appId: "1:1089570942852:web:a84062767a425c064677d1",
-    measurementId: "G-HTTDD00TTT"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
